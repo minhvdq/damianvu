47fef2f4ed025458977ac084e2d9742a3e808030
Damian@Vu <minhvdq@gmail.com>
2024-01-26 11:03:37 -0500

    debugging1

--- mySiteFE/blogs/blog-container/main.js
+++ mySiteFE/blogs/blog-container/main.js
@@ -2,6 +2,6 @@ const phoneOption = document.getElementById("option-button")
 const buttons = document.getElementById("buttons")
 phoneOption.addEventListener("click", function() {
     
-    buttons.style.display = buttons.style.display === 'none' ? 'block': 'none'
+    buttons.style.display = buttons.style.display !== 'block' ? 'block': 'none'
     console.log('hi')
 })
