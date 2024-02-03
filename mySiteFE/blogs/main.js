f70c2436ffa2720203c23859a41f49b2d5f845bc
Damian@Vu <minhvdq@gmail.com>
2024-02-03 16:09:17 -0500

    fix blog

--- mySiteFE/blogs/main.js
+++
@@ -1,7 +0,0 @@
-const phoneOption = document.getElementById("option-button")
-const buttons = document.getElementById("buttons")
-phoneOption.addEventListener("click", function() {
-    
-    buttons.style.display = buttons.style.display !== 'block' ? 'block': 'none'
-    console.log('hi')
-})
